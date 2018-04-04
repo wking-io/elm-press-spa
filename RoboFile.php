dbac843947bf7b274c2bf169454465ed9b46f13b
Will King <contact@wking.io>
2018-04-04 10:43:58 -0500

    Adding codesniffer and fixing errors

--- RoboFile.php
+++ RoboFile.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * File that defines tasks to run in Robo commands
+ *
+ * @package elm-press
+ */
+
 define( 'PROJECT_DIR', dirname( __FILE__ ) );
 define( 'TMP_DIR', PROJECT_DIR . '/tmp' );
 define( 'WP_DIR', PROJECT_DIR . '/wordpress' );
@@ -10,16 +16,21 @@ define( 'WP_DIR', PROJECT_DIR . '/wordpress' );
  */
 class RoboFile extends \Robo\Tasks {
 
-	public function wordpressSetup( $opts = [
-		'wp-user' => 'nedstark',
-		'wp-pw' => 'winteriscoming',
-		'wp-theme-dir' => 'postlight-headless-wp',
-		'wp-theme-name' => 'Postlight Headless WP Starter',
-		'wp-email' => 'nedstark@headlesswpstarter.dev',
-		'wp-db-name' => 'wp_headless',
-		'wp-description' => 'Just another (headless) WordPress site',
-		'wp-plugins' => array(),
-	] ) {
+	/**
+	 * Function that runs initial WordPress setup
+	 *
+	 * @param array $opts Config array.
+	 */
+	public function wordpressSetup( $opts = array(
+		'wp-user'        => 'nedstark',
+		'wp-pw'          => 'winteriscoming',
+		'wp-theme-dir'   => 'elm-press',
+		'wp-theme-name'  => 'elm-press',
+		'wp-email'       => 'nedstark@headlesswpstarter.dev',
+		'wp-db-name'     => 'wp_headless',
+		'wp-description' => 'Just another (headless) WordPress site that has an Elm Front End',
+		'wp-plugins'     => array(),
+	) ) {
 		$confirm = $this->io()->confirm( 'This will replace your current ' .
 		'WordPress install. Are you sure you want to do this?', false );
 
@@ -59,40 +70,37 @@ class RoboFile extends \Robo\Tasks {
 		$this->wp( 'plugin delete akismet' );
 		$this->wp( 'plugin delete hello' );
 
-		if ( is_array( $opts['wp-plugins'] ) && sizeof( $opts['wp-plugins'] ) > 0 ) {
+		if ( is_array( $opts['wp-plugins'] ) && count( $opts['wp-plugins'] ) > 0 ) {
 			$installed_plugin_directories = $opts['wp-plugins'];
 		} else {
-			$installed_plugins = array_filter( glob( WP_DIR . '/wp-content/plugins/*' ), 'is_dir' );
+			$installed_plugins            = array_filter( glob( WP_DIR . '/wp-content/plugins/*' ), 'is_dir' );
 			$installed_plugin_directories = array_filter( str_replace( WP_DIR . '/wp-content/plugins/', '', $installed_plugins ) );
 		}
 
-		if ( sizeof( $installed_plugin_directories ) > 0 ) {
+		if ( count( $installed_plugin_directories ) > 0 ) {
 			$plugins_command = 'plugin activate ' . ( implode( ' ', $installed_plugin_directories ) );
 			$this->wp( $plugins_command );
 		}
 
-		// Sync ACF
+		// Sync ACF.
 		$this->wp( 'acf sync' );
 
-		// Pretty URL structure required for wp-json path to work correctly
+		// Pretty URL structure required for wp-json path to work correctly.
 		$this->wp( 'rewrite structure "/%year%/%monthnum%/%day%/%postname%/"' );
 
-		// Set the site description
+		// Set the site description.
 		$this->wp( 'option update blogdescription "' . $opts['wp-description'] . '"' );
 
-		// Update the Hello World post
-		$this->wp( 'post update 1 wp-content/themes/postlight-headless-wp/post-content/sample-post.txt '.
-			'--post_title="Sample Post" --post_name=sample-post' );
+		// Update the Hello World post.
+		$this->wp( 'post update 1 wp-content/themes/elm-press/post-content/sample-post.txt --post_title="Sample Post" --post_name=sample-post' );
 
-		// Create homepage content
-		$this->wp( 'post create wp-content/themes/postlight-headless-wp/post-content/welcome.txt '.
-			'--post_type=page --post_status=publish --post_name=welcome '.
-			'--post_title="Congratulations!"' );
+		// Create homepage content.
+		$this->wp( 'post create wp-content/themes/elm-press/post-content/welcome.txt --post_type=page --post_status=publish --post_name=welcome --post_title="Congratulations!"' );
 
-		// Update the default 'Uncategorized' category name to make it more menu-friendly
+		// Update the default 'Uncategorized' category name to make it more menu-friendly.
 		$this->wp( 'term update category 1 --name="Sample Category"' );
 
-		// Set up example menu
+		// Set up example menu.
 		$this->wp( 'menu create "Header Menu"' );
 		$this->wp( 'menu item add-post header-menu 1' );
 		$this->wp( 'menu item add-post header-menu 2' );
@@ -103,14 +111,22 @@ class RoboFile extends \Robo\Tasks {
 		$this->io()->success( 'Great. You can now log into WordPress at: http://localhost:8080/wp-admin (' . $opts['wp-user'] . '/' . $opts['wp-pw'] . ')' );
 	}
 
+	/**
+	 * Run wp-cli command "server".
+	 */
 	public function server() {
 		$this->wp( 'server' );
 	}
 
-	public function wordpressImport( $opts = [
+	/**
+	 * Run import from specified WordPress install using migrate db pro.
+	 *
+	 * @param array $opts Config array with migrate db pro settings.
+	 */
+	public function wordpressImport( $opts = array(
 		'migratedb-license' => null,
-		'migratedb-from' => null,
-	] ) {
+		'migratedb-from'    => null,
+	) ) {
 		if ( isset( $opts['migratedb-license'] ) ) {
 			$this->wp( 'migratedb setting update license ' . $opts['migratedb-license'] );
 		} else {
@@ -119,13 +135,13 @@ class RoboFile extends \Robo\Tasks {
 		}
 
 		if ( isset( $opts['migratedb-from'] ) ) {
-			$command = 'WPMDB_EXCLUDE_RESIZED_MEDIA=1 wp migratedb pull ';
+			$command  = 'WPMDB_EXCLUDE_RESIZED_MEDIA=1 wp migratedb pull ';
 			$command .= $opts['migratedb-from'];
 			$command .= ' --backup=prefix ';
 			$command .= ' --media=compare ';
 			$this->io()->success( 'About to run data migration from ' . $opts['migratedb-from'] );
 			$this->taskExec( $command )->run();
-			// Set siteurl and home
+			// Set siteurl and home.
 			$this->wp( 'option update siteurl http://localhost:8080' );
 			$this->wp( 'option update home http://localhost:8080' );
 		} else {
@@ -134,10 +150,15 @@ class RoboFile extends \Robo\Tasks {
 		}
 	}
 
+	/**
+	 * Run wp-cli command based on passed in arg
+	 *
+	 * @param str $arg Argument to run in WP-CLI command.
+	 */
 	public function wp( $arg ) {
 		$this->taskExec( 'wp' )
-		 ->dir( WP_DIR )
-		 ->rawArg( $arg )
-		 ->run();
+		->dir( WP_DIR )
+		->rawArg( $arg )
+		->run();
 	}
 }
