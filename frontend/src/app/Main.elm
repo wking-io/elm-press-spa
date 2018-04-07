9ef2936b4eac9e1c450415eb4ff2fd513ddabf77
Will King <contact@wking.io>
2018-04-07 10:14:11 -0500

    Got initial API request setup in Elm

--- frontend/src/app/Main.elm
+++ frontend/src/app/Main.elm
@@ -1,19 +1,36 @@
 module Main exposing (main)
 
 import Html exposing (Html, program, h1, text)
+import Json.Decode as Decode exposing (Decoder, field, at, string, decodeString)
+import Http
 
 
 -- MODEL --
 
 
 type alias Model =
-    { name : String
+    { message : String
     }
+
+
+atEndpoint : String -> String
+atEndpoint endpoint =
+    "http://127.0.0.1:8080/wp-json" ++ endpoint
+
+
+welcomeDecoder : Decoder String
+welcomeDecoder =
+    at [ "title", "rendered" ] string
+
+
+initCmd : Cmd Msg
+initCmd =
+    Http.send FetchData (Http.get (atEndpoint "/elm-press/v1/page?slug=welcome") welcomeDecoder)
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "friend", Cmd.none )
+    ( Model "friend", initCmd )
 
 
 
@@ -22,7 +39,7 @@ init =
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text ("Hello, " ++ model.name) ]
+    h1 [] [ text model.message ]
 
 
 
@@ -30,14 +47,31 @@ view model =
 
 
 type Msg
-    = Default
+    = FetchData (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Default ->
-            ( model, Cmd.none )
+        FetchData (Ok response) ->
+            ( { model | message = response }, Cmd.none )
+
+        FetchData (Err httpError) ->
+            case httpError of
+                Http.BadUrl _ ->
+                    ( { model | message = "badurl" }, Cmd.none )
+
+                Http.Timeout ->
+                    ( { model | message = "timeout" }, Cmd.none )
+
+                Http.NetworkError ->
+                    ( { model | message = "networkerr" }, Cmd.none )
+
+                Http.BadStatus response ->
+                    ( { model | message = response.body }, Cmd.none )
+
+                Http.BadPayload _ _ ->
+                    ( { model | message = "badpayload" }, Cmd.none )
 
 
 
